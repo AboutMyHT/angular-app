e8f971bd96ab2353f63e3aab3900c63e00b0d71b
Steven Hogue <softdoghnut@gmail.com>
2024-03-08 18:52:35 -0600

    Added python libraries again, compiled for Linux

--- aboutmyhometown-lambda-stack/dependencies/python/amht_custom/db_utils/users.py
+++ aboutmyhometown-lambda-stack/dependencies/python/amht_custom/db_utils/users.py
@@ -9,6 +9,96 @@ from sqlalchemy.orm import Session
 logger = logging.getLogger()
 
 
+def get_user(
+    session: Session,
+    user_email: str,
+) -> User:
+    """
+    Get a user from the database by email
+
+    Return the user object if found
+
+    Raises ValueError if the user is not found
+    """
+
+    user = session.query(User).filter((User.email == user_email)).first()
+
+    if user is None:
+        raise ValueError("User not found!")
+
+    return user
+
+
+def check_password(
+    session: Session,
+    user_email: str,
+    password_rawstring: str,
+) -> bool:
+    """
+    Get a user from the database by email
+
+    Return true if the password is correct, false otherwise
+
+    Raises ValueError if the user is not found
+    """
+
+    user = get_user(session=session, user_email=user_email)
+
+    if user is None:
+        raise ValueError("User not found!")
+
+    if not bcrypt.checkpw(password_rawstring.encode("utf-8"), user.password_hash):
+        return False
+
+    return True
+
+
+def modify_user(
+    session: Session,
+    email: str,
+    password_rawstring: str = None,
+    zip_code: str = None,
+    first_name: str = None,
+    last_name: str = None,
+    email_verified: bool = False,
+    needs_password_reset: bool = False,
+    forgot_password_token: str = None,
+):
+    """
+    Modify a user in the database
+
+    return the user object if successful, raise ValueError otherwise
+    """
+    user = get_user(session, email)
+
+    if password_rawstring is not None:
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password_rawstring.encode("utf-8"), salt)
+        user.password_hash = hashed_password
+
+    if zip_code is not None:
+        user.zip_code = html.escape(zip_code)
+
+    if first_name is not None:
+        user.first_name = html.escape(first_name)
+
+    if last_name is not None:
+        user.last_name = html.escape(last_name)
+
+    if email_verified is not None:
+        user.email_verified = email_verified
+
+    if needs_password_reset is not None:
+        user.needs_password_reset = needs_password_reset
+
+    if forgot_password_token is not None:
+        user.forgot_password_token = forgot_password_token
+
+    session.commit()
+
+    return user
+
+
 def create_user(
     session: Session,
     email: str,
@@ -19,9 +109,11 @@ def create_user(
     email_verified: bool = False,
     needs_password_reset: bool = False,
     forgot_password_token: str = None,
-):
+) -> User:
     """
-    Execute the query on the MySQL database
+    Create a new user in the database
+
+    return the user object if successful, raise ValueError otherwise
     """
 
     user_email_sanitized = html.escape(email) if email else None
@@ -37,7 +129,7 @@ def create_user(
         raise ValueError("User already exists. Please use a different email.")
 
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password_rawstring, salt)
+    hashed_password = bcrypt.hashpw(password_rawstring.encode("utf-8"), salt)
 
     try:
         new_user = User(
@@ -56,10 +148,4 @@ def create_user(
     session.add(new_user)
     session.commit()
 
-    user_str = {
-        "name": f"{new_user.first_name} {new_user.last_name}",
-        "email": new_user.email,
-        "zip_code": new_user.zip_code,
-    }
-
-    return user_str
+    return new_user
--- aboutmyhometown-lambda-stack/hello_world/app.py
+++ aboutmyhometown-lambda-stack/hello_world/app.py
@@ -2,7 +2,12 @@ import logging
 
 from amht_custom import get_rds_session
 from amht_custom.http_utils import invalid_response, valid_response
-from amht_custom.db_utils.users import create_user
+from amht_custom.db_utils.users import (
+    create_user,
+    get_user,
+    check_password,
+    modify_user,
+)
 
 
 logger = logging.getLogger()
@@ -26,21 +31,47 @@ def lambda_handler(event, context):
 
     try:
         with get_rds_session()() as session:
-            return valid_response(
-                {
-                    "new_user": create_user(
-                        session=session,
-                        email=get_parameters["email"],
-                        zip_code=get_parameters["zip_code"],
-                        password_rawstring=get_parameters["password"],
-                        first_name=get_parameters.get("first_name"),
-                        last_name=get_parameters.get("last_name"),
-                    )
-                },
-                200,
+            new_user = create_user(
+                session=session,
+                email=get_parameters["email"],
+                zip_code=get_parameters["zip_code"],
+                password_rawstring=get_parameters["password"],
+                first_name=get_parameters.get("first_name"),
+                last_name=get_parameters.get("last_name"),
             )
+
+            user_in_db = get_user(session, get_parameters["email"])
+
+            modify_user(
+                session=session,
+                email=get_parameters["email"],
+                zip_code="12394",
+                first_name="John",
+                last_name="Doe",
+            )
+
+            if new_user:
+                return valid_response(
+                    {
+                        "message": "User created successfully",
+                        "user": {
+                            "email": user_in_db.email,
+                            "first_name": user_in_db.first_name,
+                            "last_name": user_in_db.last_name,
+                            "zip_code": user_in_db.zip_code,
+                        },
+                        "password_correct": check_password(
+                            session,
+                            get_parameters["email"],
+                            get_parameters["password"],
+                        ),
+                    },
+                    200,
+                )
     except ValueError as error:
         return invalid_response(
             str(error),
             400,
         )
+
+    return invalid_response("An unexpected error occured!", 500)
