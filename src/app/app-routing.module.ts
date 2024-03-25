ae0b85cc830a86bcff162b604d3b8aaeafb01955
Steven Hogue <shogue2@uis.edu>
2024-03-25 17:56:37 -0500

    Added page not found route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { SigninComponent } from './signin/signin.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { WeatherComponent } from './weather/weather.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'signin', component: SigninComponent }
+  { path: 'signin', component: SigninComponent },
+  { path: '**', component: PageNotFoundComponent }
 ]
 
 @NgModule({
