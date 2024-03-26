dd8318ced67851e5332eae5ad4c35601fea16b03
Steven Hogue <shogue2@uis.edu>
2024-03-25 21:08:47 -0500

    Fixed CORS in Lambda

--- aboutmyhometown-lambda-stack/dependencies/python/amht_custom/http_utils.py
+++ aboutmyhometown-lambda-stack/dependencies/python/amht_custom/http_utils.py
@@ -1,8 +1,16 @@
 import json
 
+allowed_origin = "https://www.aboutmyhometown.com"
+
 
 def api_response(return_obj, status_code: int = 200):
     return {
         "statusCode": status_code,
+        "headers": {
+            "Access-Control-Allow-Origin": allowed_origin,
+            "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            # "Access-Control-Allow-Credentials": "true",
+        },
         "body": json.dumps(return_obj),
     }
