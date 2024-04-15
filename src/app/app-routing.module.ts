1d9abbe6003fb9cfb88dd898de891659e3aadaba
Steven Hogue <shogue2@uis.edu>
2024-04-14 23:12:22 -0500

    Basic About Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { authGuard } from './auth.guard';
 import { guestGuard } from './guest.guard';
 
 import { SigninComponent } from './signin/signin.component';
+import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { WeatherComponent } from './weather/weather.component';
@@ -16,6 +17,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'about', component: AboutComponent },
   { path: 'signin', component: SigninComponent, canActivate: [guestGuard] },
   { path: '**', component: PageNotFoundComponent }
 ]
