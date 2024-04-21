9c5b6a39ddbd30d9d15603be542543fcb681d5d7
William Morgan <billm@wamusa.com>
2024-04-21 18:35:23 -0500

    updated to forcat page to use query prams

--- src/app/weather/forecast/forecast.component.ts
+++ src/app/weather/forecast/forecast.component.ts
@@ -5,6 +5,7 @@ import { UserService } from 'src/app/user.service';
 import { HttpClient } from '@angular/common/http';
 import { DataService } from 'src/app/data.service';
 import { Response } from 'src/app/response';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-forecast',
@@ -15,13 +16,19 @@ export class ForecastComponent implements OnInit {
   zipCode: number;
   weather: WeatherData | null = null;
 
-  constructor(private userService: UserService, private data: DataService) {
+  constructor(private userService: UserService, private data: DataService, private route: ActivatedRoute) {
     this.zipCode = this.userService.currentUser?.fiveDigitZip()!;
   }
 
 
   ngOnInit(): void {
-    this.data.getWeather(this.zipCode).subscribe(res => {
+    this.route.queryParams.subscribe(params => {
+      this.zipCode = params['zipCode'];
+      if (!this.zipCode) {
+        window.location.href = "/"
+      }
+    })
+  this.data.getWeather(this.zipCode).subscribe(res => {
       var response: Response = <Response>res;
       this.weather = <WeatherData>(JSON.parse(response.body.Item.data));
       console.log(this.weather);
